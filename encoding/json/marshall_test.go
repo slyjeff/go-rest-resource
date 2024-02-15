d8bea6b54b6acfd1f8ce3493d401ecaddbdff767
jeff.sylvester <jeff.sylvester@prizepicks.com>
2024-02-15 15:21:18 -0500

    Added tests for json encoding

--- encoding/json/marshall_test.go
+++ encoding/json/marshall_test.go
@@ -14,13 +14,16 @@ type item struct {
 	Price       float64
 }
 
-func newTestItem() item {
+func newTestItem1() item {
 	return item{"widget", 15, true, 45.2531}
 }
+func newTestItem2() item {
+	return item{"thingy", 7, false, 13.84}
+}
 
-func Test_MarshalingMustEncodeAllProperties(t *testing.T) {
+func Test_MarshalMustEncodeAllProperties(t *testing.T) {
 	//arrange
-	i := newTestItem()
+	i := newTestItem1()
 
 	var r resource.Resource
 	r.MapAllDataFrom(i)
@@ -35,9 +38,9 @@ func Test_MarshalingMustEncodeAllProperties(t *testing.T) {
 	a.Equal(expectedJson, string(json), "json not created properly.")
 }
 
-func Test_MarshalingMustEncodeFormattedData(t *testing.T) {
+func Test_MarshalMustEncodeFormattedData(t *testing.T) {
 	//arrange
-	i := newTestItem()
+	i := newTestItem1()
 
 	var r resource.Resource
 	r.MapDataFrom(i).
@@ -50,5 +53,51 @@ func Test_MarshalingMustEncodeFormattedData(t *testing.T) {
 	a := assert.New(t)
 	a.True(ok)
 	expectedJson := `{"Price":45.25}`
+	a.Equal(expectedJson, string(json), "json not created properly.")
+}
+
+func Test_MarshalMustEncodeChildStructs(t *testing.T) {
+	//arrange
+	receipt := struct {
+		Total float64
+		Item  item
+	}{
+		45.25,
+		newTestItem1(),
+	}
+
+	var r resource.Resource
+	r.MapAllDataFrom(receipt)
+
+	//act
+	json, ok := Marshal(r)
+
+	//assert
+	a := assert.New(t)
+	a.True(ok)
+	expectedJson := `{"Item":{"IsAvailable":true,"Name":"widget","Price":45.2531,"Quantity":15},"Total":45.25}`
+	a.Equal(expectedJson, string(json), "json not created properly.")
+}
+
+func Test_MarshalMustEncodeChildSlice(t *testing.T) {
+	//arrange
+	receipt := struct {
+		Total float64
+		Items []item
+	}{
+		45.25,
+		[]item{newTestItem1(), newTestItem2()},
+	}
+
+	var r resource.Resource
+	r.MapAllDataFrom(receipt)
+
+	//act
+	json, ok := Marshal(r)
+
+	//assert
+	a := assert.New(t)
+	a.True(ok)
+	expectedJson := `{"Items":[{"IsAvailable":true,"Name":"widget","Price":45.2531,"Quantity":15},{"IsAvailable":false,"Name":"thingy","Price":13.84,"Quantity":7}],"Total":45.25}`
 	a.Equal(expectedJson, string(json), "json not created properly.")
 }
